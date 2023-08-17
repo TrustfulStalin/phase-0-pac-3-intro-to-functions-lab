4f61a43af80a3a79e457ee751f515c30822311cf
Tevon Talbert <TevonTalbert@hotmail.com>
2023-08-17 13:17:36 -0500

    adding file for test

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(String) {
+  return String.toUpperCase();
+}
+
+function whisper(String) {
+  return String.toLowerCase();
+}
+
+function logShout(String) {
+  const greet = "Hello";
+  const caps = greet.toUpperCase();
+  return console.log(caps);
+}
+function logWhisper(String) {
+  const greet = "Hello";
+  const caps = greet.toLowerCase();
+  return console.log(caps);
+}
+function sayHiToHeadphonedRoommate(string) {
+  let res = "I can't hear you!";
+  let bill = string.toLowerCase;
+
+  if (string = string) {
+    return "I can't hear you!";
+  }
+
+  else if (string === string.toUpperCase) {
+    return "YES, INDEED!";
+  }
+
+  else if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+
+
+    }
+ 
